daa72d0aff4e42ba2e770330e047ebb39ea8559d
dfaulkner1012 <derek_joseph06@hotmail.com>
2020-03-14 19:37:45 -0700

    updated code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-	    countAndSay(10);
+        System.out.println(numOfStepsToZero(4));
+//	    countAndSay(10);
     }
 
-    public static void countAndSay(int numOfLines) {
-        String output = "1";
-        for (int i = 1; i <= numOfLines; i++) {
-            System.out.println(output);
-            output += "1";
-        }
+    public static int numOfStepsToZero(int currentNum) {
+        if (currentNum == 1)
+            return 0;
+        else if (currentNum % 2 == 0)
+            return 1 + numOfStepsToZero(currentNum / 2);
+        else
+            return 1 + Math.min(numOfStepsToZero(currentNum - 1),
+                    numOfStepsToZero(currentNum + 1));
     }
+
+
+//    public static void countAndSay(int numOfLines) {
+//        String output = "1";
+//        for (int i = 1; i <= numOfLines; i++) {
+//            System.out.println(output);
+//            output += "1";
+//        }
+//    }
 }
