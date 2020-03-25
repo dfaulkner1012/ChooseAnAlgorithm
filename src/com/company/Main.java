661074b91e95d4b37fdf1c47e405b20e44995c10
dfaulkner1012 <derek_joseph06@hotmail.com>
2020-03-24 20:46:18 -0700

    updated code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,29 +1,31 @@
 package com.company;
 
 public class Main {
-
+    /*
+    This answer doesn't quite work. Here's the logic you need:
+    You need an integer to represent the count for the number of steps.
+    You need a while loop that increments the count for each step.
+    This should be enough to get you started. Let me know if you need more help.
+     */
     public static void main(String[] args) {
-
-        System.out.println(numOfStepsToZero(4));
-//	    countAndSay(10);
+        System.out.println(numOfStepsToZero(14));
+        System.out.println(numOfStepsToZero(8));
+        System.out.println(numOfStepsToZero(123));
     }
 
-    public static int numOfStepsToZero(int currentNum) {
-        if (currentNum == 1)
-            return 0;
-        else if (currentNum % 2 == 0)
-            return 1 + numOfStepsToZero(currentNum / 2);
-        else
-            return 1 + Math.min(numOfStepsToZero(currentNum - 1),
-                    numOfStepsToZero(currentNum + 1));
+    public static int numOfStepsToZero(int num) {
+        int steps = 0;
+        while (num != 0) {
+            if (num % 2 == 0) {
+                num = num / 2;
+                steps++;
+            }
+            else {
+                num = num - 1;
+                steps++;
+            }
+        }
+
+        return steps;
     }
-
-
-//    public static void countAndSay(int numOfLines) {
-//        String output = "1";
-//        for (int i = 1; i <= numOfLines; i++) {
-//            System.out.println(output);
-//            output += "1";
-//        }
-//    }
 }
